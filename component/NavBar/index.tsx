9e1cfdf2a47b387824f95e9f3d1a2067482e0f02
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2025-06-24 03:00:25 +0530

    label name change

--- component/NavBar/index.tsx
+++ component/NavBar/index.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 const NavBar = () => {
   return (
     <div className="fixed max-w-7xl top-0 w-full flex justify-between items-center bg-neutral-700 text-white px-4 py-2 z-30">
-      <div className="text-2xl w-40 flex justify-center py-1 bg-gradient-to-l from-fuchsia-500 to-green-400 rounded-2xl">
-        BLOG
-        <span className="bg-white rounded-2xl text-amber-500 px-2">CMS</span>
+      <div className="text-2xl flex justify-center px-2 py-1 bg-gradient-to-l from-fuchsia-500 to-green-400 rounded-2xl">
+        Headless
+        <span className="bg-white rounded-2xl text-amber-500 px-2">BLOG</span>
       </div>
       <div>
         <ul className="list-none flex items-center gap-6">
@@ -21,11 +21,11 @@ const NavBar = () => {
         <Link href="/contact-us">Contact Us</Link>
         <a
           href="https://sanity-headless.netlify.app/"
-          className="px-4 py-2 bg-green-500 text-white font-bold rounded-3xl"
+          className="px-4 py-2 bg-violet-400 text-white font-bold rounded-3xl"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Blog-CMS
+          Launch CMS
         </a>
       </div>
     </div>
